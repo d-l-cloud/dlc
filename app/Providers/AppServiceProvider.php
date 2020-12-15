98e831171aa3469d4d67c249ffc75802c9690faf
dabsu <dabsurd@my.com>
2020-12-15 15:31:20 +0300

    v.0.0.0.3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         //Cache::forever('settings', Settings::all()); включить после
     }
 }
