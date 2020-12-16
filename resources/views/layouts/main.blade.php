c4e1adfc85fc7e6d300b50607a5432a0c728181b
dabsu <dabsurd@my.com>
2020-12-16 13:19:27 +0300

    v.0.0.0.5

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,3 +1,4 @@
+@if ($_SERVER['SERVER_NAME']=='d-l.cloud')
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -324,5 +325,6 @@
 <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(36936, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true, ecommerce:"dataLayer" }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/36936" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 </body>
 </html>
+@endif
 
 
