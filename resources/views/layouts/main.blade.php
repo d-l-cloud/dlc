df4926beed061327d7972a2f3aa40bccfe8b067b
dabsu <dabsurd@my.com>
2020-12-16 14:14:24 +0300

    v.0.0.0.5

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,4 +1,4 @@
-@if (($_SERVER['SERVER_NAME']!='d-l.cloud') OR (Helper::siteSettings('maintenance','1')!=1))
+@if (($_SERVER['SERVER_NAME']!='d-l.cloud') OR (Helper::siteSettings('maintenance','1')==1))
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
