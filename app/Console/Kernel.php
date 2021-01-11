b64ea116615f9609b1fb298ad398a59597359bfe
dabsu <dabsurd@my.com>
2021-01-11 21:28:17 +0300

    v.0.0.0.5

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,10 +25,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('getdata:dltocloud')->withoutOverlapping();
-        $schedule->command('dlcloud:sendFormEmail')->withoutOverlapping();
-        $schedule->command('scout:flush "App\Models\Shop\ProductList"')->dailyAt('02:54');
-        $schedule->command('scout:import "App\Models\Shop\ProductList"')->withoutOverlapping();
+        $schedule->command('getdata:dltocloud --domain=doorlock52.ru')->withoutOverlapping();
+        $schedule->command('dlcloud:sendFormEmail --domain=doorlock52.ru')->withoutOverlapping();
+        $schedule->command('scout:flush "App\Models\Shop\ProductList" --domain=doorlock52.ru')->withoutOverlapping();
+        $schedule->command('scout:import "App\Models\Shop\ProductList" --domain=doorlock52.ru')->withoutOverlapping();
     }
     /**
      * Register the commands for the application.
