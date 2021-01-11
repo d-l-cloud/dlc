9e48a69ed325ee178232021b3f9de1d5d0cf3ab2
dabsu <dabsurd@my.com>
2021-01-11 15:04:23 +0300

    v.0.0.0.5

--- app/Http/Controllers/Admin/Settings/ASiteSettingsController.php
+++ app/Http/Controllers/Admin/Settings/ASiteSettingsController.php
@@ -26,7 +26,6 @@ class ASiteSettingsController extends Controller
         $settings->user_id  = $request->user_id;
         $settings->updated_at = now();
         $settings->save();
-        echo $request->contactEmail;
         //return redirect()->route('admin.settings.site')->with('success','Настройки сохранены');
     }
 }
