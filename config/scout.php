65f1ba0b61eb26fcb9cfdfd51b25e9cb3e9a583a
dabsu <dabsurd@my.com>
2021-01-12 13:13:34 +0300

    v.0.0.0.5

--- config/scout.php
+++ config/scout.php
@@ -103,7 +103,7 @@ return [
         'secret' => env('ALGOLIA_SECRET', 'def563b304fb0271bf31bd93ed8cd0e9'),
     ],
     'tntsearch' => [
-        'storage'  => storage_path(), //place where the index files will be stored
+        'storage'  => storage_path(env('APP_PUBLIC_STORAGE')), //place where the index files will be stored
         'fuzziness' => env('TNTSEARCH_FUZZINESS', false),
         'fuzzy' => [
             'prefix_length' => 2,
